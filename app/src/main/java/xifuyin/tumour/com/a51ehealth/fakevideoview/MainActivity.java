551a5bb19a9ca0d3a93ad95fd0a3f1b89046faf0
XiFanYin <807142217@qq.com>
2018-04-17 09:20:13 +0800

    Initial commit

---
+++ app/src/main/java/xifuyin/tumour/com/a51ehealth/fakevideoview/MainActivity.java
@@ -0,0 +1,13 @@
+package xifuyin.tumour.com.a51ehealth.fakevideoview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
